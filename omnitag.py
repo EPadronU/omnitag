f36afeb066f2d772b8c2fd3a576780c985355489
Edinson Padrón Urdaneta <edinson.padron.urdaneta@gmail.com>
2013-08-18 14:32:57 -0430

    omnitag refactored & impromptu added & tag refresh implemented

--- omnitag.py
+++ omnitag.py
@@ -2,7 +2,7 @@
 import json
 
 from flask import Flask
-#from flask import make_response
+from flask import jsonify
 from flask import request
 from flask import render_template
 from flask_peewee.db import Database
@@ -11,8 +11,8 @@ from peewee import *
 
 # General configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 DATABASE = {
-        'name': 'devel.db',
-        'engine': 'peewee.SqliteDatabase',
+    'name': 'devel.db',
+    'engine': 'peewee.SqliteDatabase',
 }
 DEBUG = True
 SECRET_KEY = 'secret'
@@ -31,73 +31,81 @@ class Resource(db.Model):
     type = CharField(max_length=5, null=False, choices=RESOURCE_TYPES, default='F')
 
     def __hash__(self):
-        return self.__repr__().__hash__()
+        return hash(repr(self))
 
     def __repr__(self):
-        return str(self.dump_as_json())
+        return repr(self.json())
 
-    def dump_as_json(self):
-        return {'id': self.id, 'name': self.name}
+    def json(self):
+        return {'id': self.id, 'name': self.name, 'type': self.type}
 
 
 class Tag(db.Model):
     name = CharField(max_length=20, null=False, index=True)
 
     def __hash__(self):
-        return self.name.__hash__()
+        return hash(repr(self))
 
     def __repr__(self):
-        return str(self.dump_as_json())
+        return repr(self.json())
 
-    def dump_as_json(self):
+    def json(self):
         return {'id': self.id, 'name': self.name}
 
     @classmethod
     def get_by_id(cls, ids):
-        return [row.id for row in cls.select().where(cls.id << ids)]
+        return [row for row in cls.select().where(cls.id << ids)]
 
     @classmethod
     def get_by_name(cls, names):
-        return [row.id for row in cls.select().where(cls.name << names)]
+        return [row for row in cls.select().where(cls.name << names)]
 
 
 class TagResource(db.Model):
     resource = ForeignKeyField(Resource)
     tag = ForeignKeyField(Tag)
 
-    def __repr__(self):
-        return str(self.dump_as_json())
+    def __hash__(self):
+        return hash(repr(self))
 
-    def dump_as_json(self):
-        return {'resource': self.resource, 'tag': self.tag}
+    def __repr__(self):
+        return repr(self.json())
+
+    def json(self):
+        return {'id': self.id, 'resource': self.resource, 'tag': self.tag}
 
     @classmethod
     def get_resources_by_tags(cls, tags):
         return set(row.resource for row in cls.select().where(cls.tag << tags))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Controllers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @app.route('/')
 def index():
     return render_template("main-view.html")
 
-@app.route("/add-tag", methods=['POST'])
-def add_tag():
+@app.route("/add-new-tag", methods=['POST'])
+def add_new_tag():
     tag_name = json.loads(request.data)
 
     if not Tag.get_by_name([tag_name]):
-        Tag.create(name=tag_name)
-        return json.dumps({"status": "sucess"})
-    return json.dumps({"status": "failure"})
+        new_tag = Tag.create(name=tag_name)
+        new_tag.save()
+        return jsonify({"status": "success", "tag-html": render_template("tag.html", tag=new_tag)})
+
+    return jsonify({"status": "failure"})
 
 @app.route("/explorer", methods=['GET', 'POST'])
 def explorer():
     if request.method == 'GET':
         return render_template("main-view.html", tags=Tag.select())
 
-    else:
-        tags_id = json.loads(request.data)
-        tags = Tag.get_by_id(tags_id)
+    elif request.method == 'POST':
+        tags_ids = json.loads(request.data)
+        tags = Tag.get_by_id(tags_ids)
         resources = TagResource.get_resources_by_tags(tags)
         return render_template("resources.html", resources=resources)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if __name__ == "__main__":
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,118 @@
 $(document).ready(function() {
-    $("#tags ul .tag").on('click', function() {
+    // Events ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    $(window).resize(refresh_tags);
+
+    $("#tags ul .tag").click(function() {
         $(this).toggleClass('active');
-        update_resources();
+        refresh_resources();
     });
 
-    $("#add-tag").on('click', function() {
-        var tag_name = prompt("Give your new tag a name");
+    $("#add-new-tag").click(function() {
+        var prompt_states = {
+            state0: {
+                title: "Give your new tag a name",
+                html: '<input id="new-tag-name" type="text" name="new-tag-name" />',
+                buttons: {"Add": true, "Cancel": false},
+                focus: ":input:first",
+                submit: function(event, value, message, formVals) {
+                    if(value) {
+                        $.ajax({
+                            type: "POST",
+                            url: "/add-new-tag",
+                            contentType: "application/json",
+                            data: JSON.stringify(formVals["new-tag-name"]),
+                        }).done(function(data) {
+                            if(data.status == 'success') {
+                                $("#tags ul .tag:first").before(data['tag-html']);
+                                refresh_tags()
+                            }
+                        });
+                    }
+                }
+            }
+        };
 
-        $.ajax({
-            type: "POST",
-            url: "/add-tag",
-            contentType: "application/json",
-            data: JSON.stringify(tag_name),
-            dataType: "html"
-        }).done(function(json) {
-            var data = JSON.parse(json);
-            alert(data.status);
-            window.location.reload();
-        });
+        var prompt_options = {
+            classes: {
+                box: "row",
+                prompt: "col-xs-6 col-sm-4"
+            }
+        };
+        $.prompt(prompt_states, prompt_options);
     });
+
+    $("#tags ul .arrow-left").click(previous_group_of_tags);
+
+    $("#tags ul .arrow-right").click(next_group_of_tags);
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    refresh_tags();
 });
 
+// Global variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var amount_of_tags_to_show = 0;
+var current_tag_index = 0;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function next_group_of_tags() {
+    var $tags = $("#tags ul .tag");
 
-function update_resources() {
+    current_tag_index += amount_of_tags_to_show;
+    while(current_tag_index > 0 && current_tag_index > $tags.length - amount_of_tags_to_show) { current_tag_index-- }
+    refresh_tags();
+}
+
+function previous_group_of_tags() {
+    var $tags = $("#tags ul .tag");
+
+    current_tag_index -= amount_of_tags_to_show;
+    while(current_tag_index < 0) { current_tag_index++ }
+    refresh_tags();
+}
+
+function refresh_tags() {
+    var $tags = $("#tags ul .tag");
+
+    if($(window).width() >= 720) {
+        amount_of_tags_to_show = 8;
+    } else {
+        amount_of_tags_to_show = 4;
+    }
+
+    $tags.each(function(index) {
+        if(current_tag_index <= index && index < (current_tag_index + amount_of_tags_to_show)) {
+            $(this).css("display", "block");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+
+    var $arrow_right = $("#tags ul .arrow-right");
+    $arrow_right.removeClass();
+    $arrow_right.addClass("arrow-right col-xs-1 col-sm-1");
+
+    if($tags.length - current_tag_index < amount_of_tags_to_show) {
+        var offset = amount_of_tags_to_show - ($tags.length - current_tag_index);
+        $arrow_right.addClass("col-sm-offset-" + offset);
+    }
+}
+
+function refresh_resources() {
+    var tags_ids = [];
     var regex = /tag-([0-9]+)/;
-    var ids = [];
 
     $("#tags ul .tag.active").each(function(index) {
-        ids[index] = $(this).children("a").attr("href").split(regex)[1];
+        tags_ids[index] = $(this).html().split(regex)[1];
     });
 
     $.ajax({
         type: "POST",
         url: "/explorer",
         contentType: "application/json",
-        data: JSON.stringify(ids),
-        dataType: "html"
+        data: JSON.stringify(tags_ids),
     }).done(function(data) {
         $("#main-section").html(data);
     });
 }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
